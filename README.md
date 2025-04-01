f716fb0b5a868bf75ba0760fa0ef4115c868f4ec
Mauricio Villegas <5780272+mauvilsa@users.noreply.github.com>
2025-04-01 07:35:37 +0200

    Fix nw-page-editor executable for latest versions of nwjs.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 nw-page-editor - Simple app for visual editing of Page XML files.
 
-Version: 2022.09.13
+Version: 2025.04.01
 
 
 # Description
--- web-app/common.inc.php
+++ web-app/common.inc.php
@@ -2,13 +2,13 @@
 /**
  * Common code to be executed by other php scripts.
  *
- * @version $Version: 2022.09.13$
+ * @version $Version: 2025.04.01$
  * @author Mauricio Villegas <mauricio_ville@yahoo.com>
  * @copyright Copyright(c) 2017-present, Mauricio Villegas <mauricio_ville@yahoo.com>
  * @license MIT License
  */
 
-$version = str_replace('Version: ','',"Version: 2022.09.13");
+$version = str_replace('Version: ','',"Version: 2025.04.01");
 $v = '?v='.$version;
 
 /// User authentication ///
